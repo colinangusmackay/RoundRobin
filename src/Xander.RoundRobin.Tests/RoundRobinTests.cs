461cdd6bf583d3ba9701e05c0432414d4ccefff6
Colin Mackay <colin.mackay@gmail.com>
2016-09-23 13:25:56 +0100

    Add basic round robin + tests

---
+++ src/Xander.RoundRobin.Tests/RoundRobinTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Xander.RoundRobin.Tests
+{
+    [TestFixture]
+    public class RoundRobinTests
+    {
+        [Test]
+        public void Returns_Null_When_There_Are_No_Items()
+        {
+            var cut = new RoundRobin<int>(new int[0]);
+            cut.GetNextItem().ShouldBe(default(int));
+        }
+
+        [Test]
+        public void Returns_Repeating_Sequence_When_Called_Repeatedly()
+        {
+            var sequence = new[] { 10, 20, 30 };
+            var cut = new RoundRobin<int>(sequence);
+
+            cut.GetNextItem().ShouldBe(10);
+            cut.GetNextItem().ShouldBe(20);
+            cut.GetNextItem().ShouldBe(30);
+            cut.GetNextItem().ShouldBe(10);
+            cut.GetNextItem().ShouldBe(20);
+            cut.GetNextItem().ShouldBe(30);
+        }
+    }
+}
---
+++ src/Xander.RoundRobin/RoundRobin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Xander.RoundRobin
+{
+    public class RoundRobin<T>
+    {
+        private readonly T[] _items;
+
+        private int _currentIndex = -1;
+
+        public RoundRobin(IEnumerable<T> items)
+        {
+            _items = items.ToArray();
+        }
+
+        public T GetNextItem()
+        {
+            if (_items.Length == 0)
+                return default(T);
+
+            _currentIndex++;
+            if (_currentIndex >= _items.Length)
+                _currentIndex = 0;
+            return _items[_currentIndex];
+        }
+    }
+}
